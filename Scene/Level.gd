467b41a46b20a617ae7d41a7815d58d148d25d95
“Andre” <alley.indie@gmail.com>
2022-05-13 23:21:12 +0200

    Bullet unshooting hitting a coliision

--- Scene/Level.gd
+++ Scene/Level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var velocity = Vector2.ZERO
 var enemyVelocity = Vector2.ZERO
+var bullet_velocity = Vector2.ZERO
 
 export(int) var speed = 100
 export(int) var enemySpeed = 50
@@ -9,6 +10,7 @@ export(int) var enemySpeed = 50
 onready var player = $YSort/Player
 onready var enemy = $YSort/Enemy
 onready var viewport = get_viewport_rect()
+onready var bullet = $Bullet
 
 func enemy_movement_to(player_position, delta):
   enemy.position += enemy.position.direction_to(player_position) * enemySpeed * delta
@@ -17,7 +19,7 @@ func get_input(delta):
   if not player:
     return
   velocity = Vector2()
-  
+
   if Input.is_action_pressed("ui_right"):
     velocity.x += 1
   if Input.is_action_pressed("ui_left"):
@@ -30,7 +32,20 @@ func get_input(delta):
   player.position += velocity
   if velocity.x or velocity.y:
     enemy_movement_to(player.position, delta)
-    
+
+  if Input.is_action_pressed("ui_accept"):
+    #print(player.position)
+    #bullet_velocity = bullet.position.direction_to(player.position)
+    #bullet.position += bullet_velocity
+    #print(bullet_velocity)
+    var space_state = get_world_2d().direct_space_state
+    #print(player.transform.origin)
+    #print(bullet.transform.origin)
+    var result = space_state.intersect_ray(bullet.transform.origin, player.transform.origin)
+    if result:
+      print("HIT")
+      result.collider.queue_free()
+
 
 func _physics_process(delta):
   get_input(delta)
@@ -41,4 +56,3 @@ func _on_PlayerArea_area_entered(area):
   player.hide()
   player.queue_free()
   player = null
-  

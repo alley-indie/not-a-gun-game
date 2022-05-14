7b21b6862d90f4f8eec8e811399ae15dffa95652
Bohdan Iakymets <biakymet@redhat.com>
2022-05-14 22:36:49 +0200

    Fixed camera

--- Scene/CameraController.gd
+++ Scene/CameraController.gd
@@ -5,5 +5,5 @@ onready var player = get_node("/root/Level/YSort/Player")
 func _process (delta):
   if not has_node('/root/Level/YSort/Player'):
     return
-  if player.position.x > 160 and player.position.x < get_viewport_rect().size.x - 160:
+  if player.position.x > 160 and player.position.x < 430:
     position.x = player.position.x

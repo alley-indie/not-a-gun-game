fa2b4b1dacbe52757d0d5127d8386ef829ba65cb
alley-indie <alley.indie@gmail.com>
2022-05-14 10:40:47 +0200

    Update main level scene

--- Scene/BulletSceneTest.gd
+++ Scene/BulletSceneTest.gd
@@ -15,15 +15,17 @@ func get_input(delta):
   if Input.is_action_just_pressed("ui_accept"):
     var space_state = get_world_2d().direct_space_state
     unshootScript.unshoot(space_state, bullet, player)
+    bullet.queue_free()
 
 func _process(delta):
   update()
 
 func _draw():
-  var from = bullet.transform.origin
-  var to = player.transform.origin
-  
-  draw_line(from, to, Color.white)
+  if is_instance_valid(bullet):
+    var from = bullet.transform.origin
+    var to = player.transform.origin
+    
+    draw_line(from, to, Color.white)
 
 func _physics_process(delta):
   get_input(delta)
--- Scripts/Features/draw_bullet_path.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func draw(canvas_item, from, to):
-  print(canvas_item)
-  canvas_item.draw_line(from.transform.origin, to.transform.origin, Color.white)

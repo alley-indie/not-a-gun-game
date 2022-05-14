ed18340a4636c7b5144e7cfa80e185509ab363f4
Bohdan Iakymets <biakymet@redhat.com>
2022-05-14 13:01:49 +0200

    Added enemy hit animation

---
+++ Scene/Levels/Enemy.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+signal hit(enemy)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#  pass
+
+
+func _on_Enemy_hit(enemy):
+  enemy.find_node("AnimationPlayer").play("EnemyHit")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+  if anim_name == "EnemyHit":
+    self.queue_free()
--- Scripts/Features/unshoot.gd
+++ Scripts/Features/unshoot.gd
@@ -3,4 +3,4 @@ extends Node
 func unshoot(space_state, unshooter, target):
   var result = space_state.intersect_ray(unshooter.transform.origin, target.transform.origin)
   if result:
-    result.collider.queue_free()
+    result.collider.emit_signal("hit", result.collider)

cd104302fcda9110ca5544ae3547c08245190ed3
Bohdan Iakymets <biakymet@redhat.com>
2022-05-14 19:55:02 +0200

    Added more enemies and fixed camera

--- Scene/CameraController.gd
+++ Scene/CameraController.gd
@@ -5,4 +5,5 @@ onready var player = get_node("/root/Level/YSort/Player")
 func _process (delta):
   if not has_node('/root/Level/YSort/Player'):
     return
-  position.x = player.position.x
+  if player.position.x > 160:
+    position.x = player.position.x

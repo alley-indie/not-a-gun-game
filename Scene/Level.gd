5dfba857cc87dccf00b7e7172572413344228a8b
alley-indie <alley.indie@gmail.com>
2022-05-14 20:43:52 +0200

    Prototype multiple bullets

--- Scene/Level.gd
+++ Scene/Level.gd
@@ -5,12 +5,20 @@ const UnshootScript = preload("res://Scripts/Features/unshoot.gd")
 onready var player = $YSort/Player
 #onready var enemy = $YSort/StaticEnemy
 onready var viewport = get_viewport_rect()
-onready var bullet = $YSort/Bullet
+onready var bullets = $YSort/Bullets
+#onready var bullet = $YSort/Bullets/Bullet
+onready var bullet_path = $BulletPath
 
 var unshootScript
+var bullets_array = []
+var bullet_index
 
 func _ready():
+  bullet_index = 0
   unshootScript = UnshootScript.new()
+  for c in bullets.get_children():
+    bullets_array.append(c)
+  bullet_path.current_bullet = bullets_array[bullet_index]
 
 func enemy_movement_to(player_position, delta):
   pass
@@ -21,10 +29,26 @@ func get_input(delta):
   if not is_instance_valid(player):
     return
   
+  if Input.is_action_just_pressed("select_right"):
+    bullet_index += 1
+    if bullet_index >= len(bullets_array):
+      bullet_index = 0
+    bullet_path.current_bullet = bullets_array[bullet_index]
+    print(bullet_index)
+
   if Input.is_action_just_pressed("ui_accept"):
-    if is_instance_valid(bullet) and is_instance_valid(player):
-      if unshootScript.unshoot(get_world_2d().direct_space_state, bullet, player):
-        bullet.queue_free()
+    if bullets_array and bullets_array[bullet_index]:
+      var b = bullets_array[bullet_index]
+      if is_instance_valid(b):
+        if unshootScript.unshoot(get_world_2d().direct_space_state, b, player):
+          b.queue_free()
+          bullets_array.remove(bullet_index)
+          bullet_index = bullets_array.size() - 1
+          if bullet_index >= 0:
+            bullet_path.current_bullet = bullets_array[bullet_index]
+    #if is_instance_valid(bullet) and is_instance_valid(player):
+      #if unshootScript.unshoot(get_world_2d().direct_space_state, bullet, player):
+        #bullet.queue_free()
 
 func _process(delta):
   if not has_node("YSort/StaticEnemy"):
--- Scene/Levels/BulletPath.gd
+++ Scene/Levels/BulletPath.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 onready var player = $"../YSort/Player"
-onready var bullet = $"../YSort/Bullet"
+#onready var bullet = $"../YSort/Bullets/Bullet"
+
+var current_bullet
 
 func _draw():
-  if is_instance_valid(bullet) and is_instance_valid(player):
-    var from = bullet.transform.origin
+  if is_instance_valid(current_bullet) and is_instance_valid(player):
+    var from = current_bullet.transform.origin
     var to = player.transform.origin
     
     draw_line(from, to, Color.white)
+  #if is_instance_valid(bullet) and is_instance_valid(player):
+    #var from = bullet.transform.origin
+    #var to = player.transform.origin
+    
+    #draw_line(from, to, Color.white)
 
 func _process(delta):
   update()

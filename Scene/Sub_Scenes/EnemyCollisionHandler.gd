fb4fb7765582e834ea8dbb4057d256ef9a4e0012
alley-indie <alley.indie@gmail.com>
2022-05-15 10:12:30 +0200

    Bullet only selectable when close to player

--- Scene/Sub_Scenes/EnemyCollisionHandler.gd
+++ Scene/Sub_Scenes/EnemyCollisionHandler.gd
@@ -2,7 +2,5 @@ extends Node2D
 
 func on_hit(collider):
   if (collider == "bullet"):
-    print("HIT")
-    print(get_parent().find_node("AnimationPlayer"))
     get_parent().find_node("AnimationPlayer").play("EnemyHit")
     #get_parent().queue_free()

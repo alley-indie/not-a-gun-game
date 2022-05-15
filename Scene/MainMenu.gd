74a2ce599a3be7f9f292d31be9684072b52cd3f7
alley-indie <alley.indie@gmail.com>
2022-05-15 14:44:34 +0200

    Win Scene

--- Scene/MainMenu.gd
+++ Scene/MainMenu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_StartButton_pressed():
-  Global.play_button_sound()
   Global.change_scene("res://Scene/Level.tscn")
+  Global.play_button_sound()
 
 func _on_QuitButton_pressed():
   Global.play_button_sound()
